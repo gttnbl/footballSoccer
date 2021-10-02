f490969cdd0d22f88487456f61fe64f3c6602a1a
unknown <gtetnabi@gmail.com>
2021-10-02 20:17:13 +0200

    test jenkins

--- src/main/java/com/footballSoccer/gate/controller/PlayerController.java
+++ src/main/java/com/footballSoccer/gate/controller/PlayerController.java
@@ -16,7 +16,7 @@ public class PlayerController {
 
 	@Autowired
 	PlayerService playerService;
-//test
+//testtest
 	@RequestMapping(value = "/all", method = RequestMethod.GET, produces={"application/json"},headers = "Accept=application/json")
 	public List<Player> gelAllPlayers() {
 
