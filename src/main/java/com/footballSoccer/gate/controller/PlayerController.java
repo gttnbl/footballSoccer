bf437bc5c80735ecedabf8733a82cf5252e4b6e9
unknown <gtetnabi@gmail.com>
2021-10-16 23:34:14 +0200

    test

--- src/main/java/com/footballSoccer/gate/controller/PlayerController.java
+++ src/main/java/com/footballSoccer/gate/controller/PlayerController.java
@@ -18,6 +18,7 @@ public class PlayerController {
 	PlayerService playerService;
 //testtestefezrgfefzefgerzergfezrgergre"
 //testtestefezrgfefzefgerzergfezrgergre"
+//testtestefezrgfefzefgerzergfezrgergre"
 	@RequestMapping(value = "/all", method = RequestMethod.GET, produces={"application/json"},headers = "Accept=application/json")
 	public List<Player> gelAllPlayers() {
 
