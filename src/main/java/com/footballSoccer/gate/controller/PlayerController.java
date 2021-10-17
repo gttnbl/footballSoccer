574d88912dc97aa22ea0cd000953f2b5aef1f395
unknown <gtetnabi@gmail.com>
2021-10-17 14:23:44 +0200

    remove comment

--- src/main/java/com/footballSoccer/gate/controller/PlayerController.java
+++ src/main/java/com/footballSoccer/gate/controller/PlayerController.java
@@ -16,8 +16,7 @@ public class PlayerController {
 
 	@Autowired
 	PlayerService playerService;
-//testtestefezrgfefzefgerevfszzerfezr
-//testtestefezrgfefzefger
+
 	@RequestMapping(value = "/all", method = RequestMethod.GET, produces={"application/json"},headers = "Accept=application/json")
 	public List<Player> gelAllPlayers() {
 
